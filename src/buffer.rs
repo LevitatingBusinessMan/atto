f475b6f379c24cf964de2371c5cc4100fbc3049c
LevitatingBusinessMan (Rein Fernhout) <me@levitati.ng>
2024-11-04 08:06:53 +0100

    fix pagedown bug

--- src/buffer.rs
+++ src/buffer.rs
@@ -139,7 +139,7 @@ impl Buffer {
     pub fn page_down(&mut self, height: usize) {
         let (col, mut row) = self.cursor_pos();
         row -= self.top as u16;
-        self.top = cmp::min(self.top + height - 1, self.content.lines().count() - height);
+        self.top = cmp::min(self.top + height - 1, self.content.lines().count().saturating_sub(height));
         self.set_viewport_cursor_pos(self.prefered_col.unwrap_or(col as usize) as u16, row);
     }
 
