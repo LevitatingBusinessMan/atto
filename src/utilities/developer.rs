8d2164b8f36aab00d5e899e9d01833ca14a23603
LevitatingBusinessMan (Rein Fernhout) <me@levitati.ng>
2025-02-21 02:30:39 +0100

    ability to suspend itself added

--- src/utilities/developer.rs
+++ src/utilities/developer.rs
@@ -1,5 +1,8 @@
 //! A utility for testing
 
+use std::io::stdout;
+
+use nix::unistd::Pid;
 use ratatui::{style::{Color, Style}, widgets::{Clear, Paragraph, Wrap}};
 use indoc::indoc;
 
@@ -12,7 +15,7 @@ impl super::Utility for DeveloperModel {
         match msg {
             Message::InsertChar(char) => {
                 match char {
-                    n => Some(Message::Notification(indoc!{"
+                    '\n' => Some(Message::Notification(indoc!{"
                         warning: unused variable: `width`
                         --> src/view.rs:139:21
                             |
@@ -22,6 +25,17 @@ impl super::Utility for DeveloperModel {
                             = note: `#[warn(unused_variables)]` on by default"}.to_owned()
                         , Style::new().bg(Color::Red)
                     )),
+                    'z' => {
+                        crossterm::execute!(stdout(), crossterm::terminal::Clear(crossterm::terminal::ClearType::All)).unwrap();
+                        crate::tui::restore().unwrap();
+
+                        // send SIGSTOP to ourselves
+                        nix::sys::signal::kill(Pid::from_raw(0), nix::sys::signal::Signal::SIGSTOP).unwrap();
+
+                        crate::tui::setup().unwrap();
+                        crate::TERMINAL.get().unwrap().lock().unwrap().clear().unwrap();
+                        Some(Message::CloseUtility)
+                    }
                     _ => None
                 }
             },
@@ -31,6 +45,7 @@ impl super::Utility for DeveloperModel {
     fn view(&self, m: &crate::model::Model, f: &mut ratatui::Frame, area: ratatui::prelude::Rect) {
         super::default_view("brrrrr", indoc! {"
         * n - create an error notification
+        * z - experiemntal suspend option
         "}, f, area);
     }
 }
