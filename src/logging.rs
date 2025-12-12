7b3c67703757f7b5d4b63dd0ddd3e48f9270d625
LevitatingBusinessMan (Rein Fernhout) <me@levitati.ng>
2025-12-12 04:23:07 +0100

    log span enters

--- src/logging.rs
+++ src/logging.rs
@@ -27,7 +27,7 @@ pub fn setup_logging(args: &crate::Args) -> io::Result<()> {
         .with_writer(file)
         .with_target(true)
         .with_ansi(true)
-        .with_span_events(FmtSpan::CLOSE);
+        .with_span_events(FmtSpan::ACTIVE);
         //.with_filter(LevelFilter::from_level(level));
 
     let subscriber = Registry::default()
