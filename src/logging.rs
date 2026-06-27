1608717ae53210b08644ba353a960169a1b50eaa
LevitatingBusinessMan (Rein Fernhout) <me@levitati.ng>
2026-06-27 18:08:20 +0200

    make logging resilient

--- src/logging.rs
+++ src/logging.rs
@@ -15,51 +15,49 @@ pub fn setup_logging(args: &crate::Args) -> io::Result<()> {
     let syslog_layer = {
         static IDENTITY: &'static CStr = c"atto";
         let (options, facility) = Default::default();
-        let writer = syslog_tracing::Syslog::new(IDENTITY, options, facility)
-            .ok_or(io::Error::other("failed to create syslog writer"))?;
-        tracing_subscriber::fmt::layer()
+        syslog_tracing::Syslog::new(IDENTITY, options, facility).map(|writer| {
+            tracing_subscriber::fmt::layer()
                 .without_time()
                 .with_target(false)
                 .with_ansi(false)
                 .with_writer(writer)
+        })
     };
-    
-    let file = fs::File::options()
-        .write(true)
-        .append(true)
-        .create(true)
-        .open(
-            args.logfile.clone().unwrap_or(
-                dirs::cache_dir().ok_or_else(|| io::Error::other("failed to find cache dir"))?
-                .join("atto.log")
-            )
-        )?;
 
-    let file_layer = tracing_subscriber::fmt::layer()
-        .with_line_number(true)
-        .with_writer(file)
-        .with_target(true)
-        .with_ansi(true)
-        .with_span_events(FmtSpan::ENTER | FmtSpan::CLOSE);
-        //.with_filter(LevelFilter::from_level(level));
+    let file_layer = {
+        let file = dirs::cache_dir()
+            .and_then(|cache_dir| fs::File::options()
+                .write(true)
+                .append(true)
+                .create(true)
+                .open(cache_dir.join("atto.log"))
+                .ok()
+            );
+        file.map(|file| tracing_subscriber::fmt::layer()
+            .with_line_number(true)
+            .with_writer(file)
+            .with_target(true)
+            .with_ansi(true)
+            .with_span_events(FmtSpan::ENTER | FmtSpan::CLOSE)
+        )
+    };
 
-    let use_log_file = true;
-
-    let (pipe_read, pipe_write) = nix::unistd::pipe()?;
-    nix::unistd::dup2(pipe_read.as_raw_fd(), 100)?;
-    let fd_file = unsafe { fs::File::from_raw_fd(pipe_write.into_raw_fd()) };
-
-    let pipe_layer = tracing_subscriber::fmt::layer()
-        .with_line_number(true)
-        .with_writer(fd_file)
-        .with_target(true)
-        .with_ansi(true)
-        .with_span_events(FmtSpan::ENTER | FmtSpan::CLOSE);
+    let pipe_layer = nix::unistd::pipe().ok().and_then(|(pipe_read, pipe_write)| {
+        nix::unistd::dup2(pipe_read.as_raw_fd(), 100).ok()?;
+        let fd_file = unsafe { fs::File::from_raw_fd(pipe_write.into_raw_fd()) };
+        let layer = tracing_subscriber::fmt::layer()
+            .with_line_number(true)
+            .with_writer(fd_file)
+            .with_target(true)
+            .with_ansi(true)
+            .with_span_events(FmtSpan::ENTER | FmtSpan::CLOSE);
+        Some(layer)
+    });
     
     let subscriber = Registry::default()
         .with(env)
         .with(syslog_layer)
-        .with(use_log_file.then_some(file_layer))
+        .with(file_layer)
         .with(pipe_layer);
 
     let _ = tracing::subscriber::set_global_default(subscriber);
